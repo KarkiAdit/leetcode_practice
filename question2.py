2b5fcef0a1d29fcebf4c61758a15d5e67b442d31
Aditya Karki <adityakarki728@gmail.com>
2022-08-24 20:37:13 -0500

    Optimized to O(n) complexity

--- question2.py
+++ question2.py
@@ -1,10 +1,22 @@
 # Finding two sum target
 
+# class Solution:
+
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i]+nums[j] == target:
+#                     return [i, j]
+
+# Optimized code in O(n) complexity
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
+        hash_map = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
+            diff = target - nums[i]
+            if diff in hash_map and i != hash_map[diff] != i:
+                return [i, hash_map[diff]]
+            else:
+                hash_map[nums[i]] = i
