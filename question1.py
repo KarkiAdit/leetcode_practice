172722511e96877d6bcc66a48a39fe989435fe9e
Aditya Karki <adityakarki728@gmail.com>
2022-08-24 21:51:54 -0500

    Updated code #Day2

--- question1.py
+++ question1.py
@@ -2,10 +2,25 @@
 
 class Solution:
     def isPowerOfThree(self, n: int):
-        x = 0
 
-        while 3**x <= n:
-            if 3**x == n:
-                return True
-            x += 1
-        return False
+        # x = 0
+        # while 3**x <= n:
+        #     if 3**x == n:
+        #         return True
+        #     x += 1
+        # return False
+
+        # if n == 0:
+        #     return True
+        # else:
+        #     if n % 3 != 0:
+        #         return False
+        #     return self.isPowerOfThree(int(n/3))
+
+        if n == 0:
+            return False
+        while n != 0 and n != 1:
+            if n % 3 != 0:
+                return False
+            n = int(n/3)
+        return True
